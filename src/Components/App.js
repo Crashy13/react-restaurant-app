d1ebd92892a145e7a7f01bca7c3f71d787906656
Scotty Wyatt <crashy@Scottys-MacBook-Pro.local>
2021-06-04 14:05:31 -0400

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return(
+      <>
+        <h1>Vic's Pizza</h1>,
+        <h2>Menu</h2>
+      </>
+    )
+  }
+}
+
+
+export default App;
---
+++ src/Components/MenuList.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class MenuList extends Component {
+
+
+  render() {
+    return
+  }
+
+  }
+
+
+export default MenuList
